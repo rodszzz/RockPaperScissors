cc59b3c8983407bdea068cefcd12a17a268b8dc5
Aurora <jakersaf@gmail.com>
2022-08-23 16:43:17 -0300

    add the files

---
+++ script.js
@@ -0,0 +1,110 @@
+// just to make my life easier
+// i just log instead of console.log
+// my wrists say thank you
+function log(thing) {
+  return console.log(thing);
+}
+
+function computerPlay() {
+  // it picks a random number between 1 and 3
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  // a list of choices to the computer to choose from
+  const computerChoice = ["rock", "paper", "scissors"];
+
+  // ok so here's the magic
+  // pick the random number -1 because
+  // the list starts at 0 and not 1
+  // so every time it gets called, new random choice!
+  return computerChoice[randomNumber - 1];
+}
+
+// tracks the score of the computer and the player
+let scorePC = 0;
+let scorePlayer = 0;
+
+function playRound(playerSelection, computerSelection) {
+  // makes the playerSelection case-insensitive and only reads the first char
+  playerSelection = playerSelection.toLowerCase().slice(0, 1);
+
+  // checks the first char so its easier to type
+  // doesnt matter typing if you type "rock" or "r" it's the same 🤠
+  if (playerSelection === "r") {
+    playerSelection = "rock";
+  } else if (playerSelection === "s") {
+    playerSelection = "scissors";
+  } else if (playerSelection === "p") {
+    playerSelection = "paper";
+  }
+
+  // only for debug purposes
+  // but i'll keep it because i think its cooler with it
+  log(`You chose: ${playerSelection}`);
+
+  // beeg switch function so it's readable
+  // easier to debug for me and not too much spaghetti
+  switch (true) {
+    case computerSelection === playerSelection:
+      log(`${computerSelection} does nothing to ${playerSelection}\nDRAW!`);
+      break;
+
+    case computerSelection === "rock" && playerSelection === "scissors":
+      log(`${computerSelection} beats ${playerSelection}\nComputer Wins!`);
+      scorePC += 1;
+      break;
+
+    case computerSelection === "rock" && playerSelection === "paper":
+      log(`${playerSelection} beats ${computerSelection}\nYou Win!`);
+      scorePlayer += 1;
+      break;
+
+    case computerSelection === "paper" && playerSelection === "scissors":
+      log(`${playerSelection} beats ${computerSelection}\nYou Win!`);
+      scorePlayer += 1;
+      break;
+
+    case computerSelection === "paper" && playerSelection === "rock":
+      log(`${computerSelection} beats ${playerSelection}\nComputer Wins!`);
+      scorePC += 1;
+      break;
+
+    case computerSelection === "scissors" && playerSelection === "rock":
+      log(`${playerSelection} beats ${computerSelection}\nYou Win!`);
+      scorePlayer += 1;
+      break;
+
+    case computerSelection === "scissors" && playerSelection === "paper":
+      log(`${computerSelection} beats ${playerSelection}\nComputer Wins!`);
+      scorePC += 1;
+      break;
+
+    default:
+      log(`ERROR\nPC:${computerSelection}\nYOU:${playerSelection}`);
+      break;
+  }
+}
+
+function game() {
+  // calls playRound to play a round
+  // use a for loop to play 5 rounds
+  for (let i = 0; i < 5; i++) {
+    // gets user input
+    let playerSelection = prompt("Rock | Paper | Scissors: ");
+    playRound(playerSelection, computerPlay());
+  }
+
+  // log the score of the player and computer
+  log(`-----------------------------\nComputer: ${scorePC}`);
+  log(`Player: ${scorePlayer}`);
+
+  // check who won and output it
+  if (scorePC > scorePlayer) {
+    log("YOU LOOOOOOOOOOOOOOOSE NEEEEEEEEEEEEEEEEEEEEERD");
+  } else if (scorePC < scorePlayer) {
+    log("Congrats dud, you win :)");
+  } else {
+    log("DRAW, best luck next time :)");
+  }
+}
+
+// finally play the game
+game();
